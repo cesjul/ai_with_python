5ab5b9b80c3c9adc3b19cf2b2b87c24a121b6ecd
cesjul <juliocesarsouzacarvalho284@gmail.com>
2025-05-19 21:05:38 -0300

    Calculadora com python

---
+++ main.py
@@ -0,0 +1,35 @@
+first_number = input('Insert a number: ')
+
+while True:
+    try:
+        float_first_number = float(first_number)
+        break
+    except ValueError:
+        first_number = input('Insert a number: ')
+        if first_number.isdigit():
+            float_first_number = float(first_number)
+            break
+        continue
+
+operation = input('Insert a operation (* / + -): ')
+
+while not operation in '- + / *':
+    operation = input('Insert a operation (* / + -): ')
+
+second_number = input('Insert a number: ')
+
+while True:
+    try:
+        float_second_number = float(second_number)
+        break
+    except ValueError:
+        second_number = input('Insert a number: ')
+        if second_number.isdigit():
+            float_second_number = float(second_number)
+            break
+        continue
+
+result = eval(f'{first_number}{operation}{second_number}')
+
+print(result)
+
