49c63f593f569ce347764524ec043b67ac041e81
cesjul <juliocesarsouzacarvalho284@gmail.com>
2025-05-19 21:25:12 -0300

    Calculadora com python

--- main.py
+++ main.py
@@ -21,15 +21,29 @@ second_number = input('Insert a number: ')
 while True:
     try:
         float_second_number = float(second_number)
+        if second_number == '0' and '/' in operation:
+                print('Cant divide by 0')
+                print('Insert a new one')
+                raise ZeroDivisionError
         break
     except ValueError:
         second_number = input('Insert a number: ')
         if second_number.isdigit():
+            
             float_second_number = float(second_number)
             break
         continue
+    except ZeroDivisionError:
+        second_number = input('Insert a number: ')
+        if second_number.isdigit() and second_number != '0':
+            
+            float_second_number = float(second_number)
+            break
+
+
 
 result = eval(f'{first_number}{operation}{second_number}')
 
 print(result)
+    
 
